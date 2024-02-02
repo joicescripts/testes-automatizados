52a38c16b1592ea34abd0925e67e4be4d161255a
Joice Martins <joicemartins.dev@gmail.com>
2024-02-02 12:08:15 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+const server = require('./src/app')
+
+const PORT = process.env.PORT
+
+mongoose.connect(process.env.MONGO_DB_URL)
+    .then(() => {
+        console.log('::: Mongo DB connected :::')
+        server.listen(PORT, () => console.log(`App listening on PORT:::${PORT}`))
+    })
+    
+module.exports = server
